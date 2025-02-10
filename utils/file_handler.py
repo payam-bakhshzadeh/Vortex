9a207842c9d3023bbc3406dfc1c6a677299f9a3e
Payam <payam.bakhshzadeh@gmail.com>
2025-02-10 21:52:14 +0330

    Update file_handler.py

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,11 +1,16 @@
-# utils/file_handler.py
 import json
 from pathlib import Path
 
 class FileHandler:
     def __init__(self, default_path="output.json"):
-        self.default_path = default_path
+        self.default_path = self.ensure_json_extension(default_path)
         self.current_path = self.load_last_path()
+
+    def ensure_json_extension(self, path):
+        """Ensure the file has a .json extension."""
+        if not path.endswith(".json"):
+            path += ".json"
+        return path
 
     def load_last_path(self):
         """Load the last saved path from a configuration file."""
@@ -13,7 +18,7 @@ class FileHandler:
         if config_path.exists():
             with config_path.open('r') as f:
                 config = json.load(f)
-                return config.get("json_save_path", self.default_path)
+                return self.ensure_json_extension(config.get("json_save_path", self.default_path))
         return self.default_path
 
     def save_last_path(self, path):
@@ -32,6 +37,7 @@ class FileHandler:
         if path is None:
             path = self.current_path
         else:
+            path = self.ensure_json_extension(path)
             self.current_path = path
             self.save_last_path(path)
         with Path(path).open('w') as f:
